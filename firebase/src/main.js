77d8d70e3788a6e785bdb33652fb13be9ad37bdd
DaxChen <tp6m4cj96@gmail.com>
2016-08-17 17:32:39 +0800

    mdl template: android added

---
+++ firebase/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+// import App from './App'
+import Landing from './Landing'
+
+/* eslint-disable no-new */
+new Vue({
+  el: 'body',
+  // components: { App }
+  components: { Landing }
+})
